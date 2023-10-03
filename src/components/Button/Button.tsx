d09e24b559df38ec07b45421e743c962e6fd2d67
Pratham <chudasamapratham142@gmail.com>
2023-10-04 00:15:36 +0530

    added more components and working perfectly

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,5 +1,11 @@
-import React from 'react';
+import React from "react";
+import "./Button.css";
 
-export default function Button() {
-	return <button>Button</button>;
+interface ButtonProps {
+	children?: React.ReactNode;
+	onClick?: () => void;
+}
+
+export default function Button({ onClick, children }: ButtonProps) {
+	return <button onClick={onClick}>{children}</button>;
 }
